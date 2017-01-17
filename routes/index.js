2d9002ea38294a42b8d623680b45e15b79ab9206
tayutaedomo <tayutaedomo@gmail.com>
2017-01-17 18:05:24 +0900

    Add webhook logging endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/webhook', function(req, res, next) {
+  console.log('request headers', req.headers);
+  console.log('request body', req.body);
+
+  res.send();
+});
+
 module.exports = router;
